4543389caf9e7acd70fef4c97231d2e1b96d81bb
Jaehui Yu <pkj0522@naver.com>
2024-10-14 14:47:09 +0900

    Initial Commit

---
+++ DearDay/DearDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  DearDayApp.swift
+//  DearDay
+//
+//  Created by Jaehui Yu on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct DearDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
